a21adb58c790ba61df98639f26c2a7d5a5583477
kelbiyev <kelbiyev11@mail.ru>
2024-09-25 10:56:11 +0400

    Add files

---
+++ pk0.cpp
@@ -0,0 +1,36 @@
+/*
+#include <iostream>
+#include <array>
+
+int main(){
+	
+	std::array<int,3> point{1 , 2 , -3};
+	
+	retunr 0;
+
+}
+*/
+
+
+#include <iostream>
+#include <deque>
+
+int main (){
+	std::deque<int> d {1, 2, 3 ,4};
+	d.push_back(5);
+	d.push_back(6);
+	d.pop_back();
+	d.push_front(0);
+	d.push_front(1);
+	d.pop_front();
+	
+	for(size_t i = 0; i != d.size() ; ++i){
+		std::cout<< d[i] <<"\n";
+	}
+	
+	for (int x : d){
+		std::cout<< x <<"\n";
+	}
+	
+	return 0;
+}
